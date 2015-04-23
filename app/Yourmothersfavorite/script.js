4e51bb95df23836aba33432a145aecaaf9ec784f
unknown <m.sahlmueller@SahlmuellerMLap.fis-gmbh.de>
2015-04-23 13:49:39 +0200

    nie mehr erste liga HSV

--- app/Yourmothersfavorite/script.js
+++ app/Yourmothersfavorite/script.js
@@ -1,12 +1,29 @@
 function zeigeFenster() {
 	  dialog.showModal();
-	}
+	};
+
 
 
 $(document).ready(function(){
  $('input').focus(function() {
      $(this).css('outline-color', 'red');
     });
+
+
+$("#button").click(function(){
+var username = $('input[name=checkListItem]').val();
+
+if (username !== "") {
+    alert('Viel Fun beim Chatten ' + username);
+    dialog.close();
+} else {
+    alert('Bitte erst Nutzernamen eingeben');
+}
+
+});
+
+
+
 });
 
 
