7a8afa8e92692ae0c1f17aaa9c4dfc4af8d41828
unknown <l.schmitt@SchmittLLap2.fis-gmbh.de>
2015-04-24 09:13:53 +0200

    big rebuild

---
+++ app/app.js
@@ -0,0 +1,19 @@
+var express = require('express');
+var fs = require('fs');
+var app = express();
+
+app.use("/css", express.static('./static/css'));
+app.use("/js", express.static('./static/js'));
+app.use("/pictures", express.static('./static/pictures'));
+
+app.get('/', function (req, res) {
+    var data = fs.readFileSync('./view/index.html').toString();
+    res.status(200).send(data);
+
+});
+
+var server = app.listen(3000, function () {
+  var host = "localhost";
+  var port = server.address().port;
+  console.log('Example app listening at http://%s:%s', host, port);
+});
