6e8af02f2c5913093303a594c061669bd2c41e5f
Nhyx3 <l.schmitt@fis-gmbh.de>
2015-05-04 16:36:46 +0200

    Persistierung von Chatnachrichten implementieren

--- app/app.js
+++ app/app.js
@@ -1,10 +1,17 @@
 var express = require('express');
 var fs = require('fs');
+
 var app = express();
+var bodyParser = require('body-parser');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({extended: true}));
 
 app.use("/css", express.static('./static/css'));
 app.use("/js", express.static('./static/js'));
 app.use("/pictures", express.static('./static/pictures'));
+
+var messages = [];
 
 app.get('/', function (req, res) {
     var data = fs.readFileSync('./view/index.html').toString();
@@ -16,6 +23,10 @@ app.get('/chatroom', function (req, res) {
     var data = fs.readFileSync('./view/Chatroom.html').toString();
     res.status(200).send(data);
 
+});
+
+app.post('/messages', function (req, res) {
+	messages.push(req.body);
 });
 
 var server = app.listen(3000, function () {
--- app/static/js/script.js
+++ app/static/js/script.js
@@ -1,96 +1,108 @@
-var nachricht =""
-var userName = ""
+var nachricht = "";
+var userName = "";
+var pollingInterval;
 
-$(document).ready(function(){
-$('#userName').focus();
-$('#message').attr("disabled", true);
-$('input').focus(function() {
-$(this).css('outline-color', 'red');
+$(document).ready(function() {
+
+	// init
+	$('#userName').focus();
+	$('#message').attr("disabled", true);
+	$('input').focus(function() {
+		$(this).css('outline-color', 'red');
+	});
+
+
+	// login handlers
+	$("#button").click(function(event) {
+		login();
+	});
+	$("#userName").keydown(function (event) {
+		if (event.which !== 13) {
+			return;
+		}
+		login();
+	});
+	$(document).on('submit', '#userForm', function (event) {
+		return false;
+	});
+
+
+	// message handlers
+	$("#senden").click(function(event) {
+		schreiben();
+	});
+	$("#message").keydown(function (event) {
+		if (event.which !== 13) {
+			return;
+		}
+		schreiben();
+	});
+	$(document).on('submit', '#löschen', function (event) {
+		return false;
+	});
+
+
+	// logout handlers
+	$("#exit").click(function(){
+		$('#userListe').remove(userName);
+	});
+
 });
 
 
-$("#senden").click(function(event) {
-schreiben();
-});
 
-$("#message").keydown(function (event) {
-if (event.which !== 13) {
-return;
-}
-schreiben();
-});
+function login () {
 
-$(document).on('submit', '#löschen', function (event) {
-return false;
-});
+	userName = $('#userName').val();
+	if (userName === "") {
+		alert('Bitte erst Nutzernamen eingeben');
+		return;
+	} else {
+		alert('Viel Fun beim Chatten ' + userName);
+		console.log("closing dialog");
+		dialog.close();
 
-$("#button").click(function(event) {
-popup();
-});
-$("#userName").keydown(function (event) {
-if (event.which !== 13) {
-return;
-}
-popup();
-});
+		$('#message').attr("disabled", false);
+		$('#message').focus(); 
 
-$("#exit").click(function(){
-$('#userListe').remove(userName);
+		$('#userListe').append(userName);
+	}
 
-});
+	$("#chatbox").append('Charles: '+userName + ' ist dem Chat beigetreten');
 
-$(document).on('submit', '#userForm', function (event) {
-return false;
-});
+	pollingInterval = setInterval(function () {
 
-
-
-
-
-});
-
-
-
-function popup () {
-
-userName = $('#userName').val();
-if (userName === "") {
-alert('Bitte erst Nutzernamen eingeben');
-return;
-}
-else {
-alert('Viel Fun beim Chatten ' + userName);
-console.log("closing dialog");
-dialog.close();
-
-$('#message').attr("disabled", false);
-$('#message').focus(); 
-
-$('#userListe').append(userName);
-}
-
-$("#chatbox").append('Charles: '+userName + ' ist dem Chat beigetreten');
-
+	}, 1000);
 
 };
 
 function schreiben () {
-nachricht = $('#message').val();
-if (nachricht === "" ) {
-	
-}
 
-
-/*var url = "";
-if (nachricht === "charles.logout") {
-	location.reload();
-}*/
-else{
+	nachricht = $('#message').val();
+	if (nachricht === "" ) {
+		return;
+	}
 
 	$("#chatbox").append('<div class="item">' + userName + ": " + nachricht + '</div>');
-$("input").val("");
+	$("input").val("");
 
-}
+	var data = {
+		user:      userName,
+		text:      nachricht,
+		timestamp: Date.now()
+	};
+	$.ajax({
+		type: "POST",
+		url: "/messages",
+		data: data
+	});
+
+	/*
+	var url = "";
+	if (nachricht === "charles.logout") {
+		location.reload();
+	}
+	*/
 };
 
 
